81431b5afe6480c0cab35b3ca116a298b42f0370
Ali Taherzadeh <Ali.Thz83@gmail.com>
2024-05-01 09:42:32 +0330

    stage title changed

--- src/main/java/com/example/ubereats/HelloApplication.java
+++ src/main/java/com/example/ubereats/HelloApplication.java
@@ -12,15 +12,10 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Welcome to Uber Eats");
         stage.setScene(scene);
         stage.show();
-
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load());
-//        stage.setTitle("UberEats");
-//        stage.setScene(scene);
-//        stage.show();
+        stage.setResizable(false);
     }
 
     public static void main(String[] args) {
